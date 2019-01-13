bb5b7b971b01ed369d27e02d165204d12e40e480
gargaryan <45004188+gargaryan@users.noreply.github.com>
2019-01-13 22:28:57 +0530

    Add files via upload

---
+++ Exponential_of_a_number.py.py
@@ -0,0 +1,6 @@
+#program to input two number and find second number is to be raised to  first number
+num1=int(input('enter a first number'))
+num2=int(input('enter a second number which is to be raised'))
+power=num1**num2;       #num2 is raised to num1
+print(power)
+#program ends
