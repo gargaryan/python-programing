ebf5fdee31c2cba9bd5affb8c4f9367dcdffc8bf
gargaryan <45004188+gargaryan@users.noreply.github.com>
2019-01-13 22:20:57 +0530

    Add files via upload

---
+++ substraction.py
@@ -0,0 +1,11 @@
+#program to substraction of two number
+num1=int(input("enter a first number"))
+num2=int(input("enter a second number"))
+if num1>num2:
+    sub=num1-num2;
+    print(sub)
+else:
+    sub=num2-num1;
+    print(sub)        #substraction of two number
+
+#program ends    
