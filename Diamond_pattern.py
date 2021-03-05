55cebf7130dc2554e9382f2e5437c51d067dcf82
Aryan garg <45004188+gargaryan@users.noreply.github.com>
2021-03-05 16:56:44 +0530

    Python PT-3_hackerrank

---
+++ Diamond_pattern.py
@@ -0,0 +1,24 @@
+row=int(input(''))
+for i in range(0,row):
+    for j in range(row,i,-1):
+        print("*",end=" ")
+    for k  in range(i+1):
+        print(' ',end=" ")
+    for l in range(i):
+        print(' ',end=" ")
+    for m in range(row,i,-1):
+        print('*',end=" ")
+    print('')
+#First loop ends
+for i in range(0,row):
+    for j in range(i+1):
+        print('*',end=" ")
+    for k in range(row,i,-1):
+        print(' ',end=" ")
+    for l in range(row,i+1,-1):
+        print(' ',end=" ")
+    for m in range(i+1):
+        print('*',end=" ")
+    print('')
+#second loop ends
+
